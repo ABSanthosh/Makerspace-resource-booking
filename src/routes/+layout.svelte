891b88a7759b40b31fce1328d7c94128a93792a3
ABSanthosh <a.b.santhosh02@gmail.com>
2023-12-29 16:39:01 +0530

    feat: Init app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>SNU Makerspace</title>
+	<meta charset="utf-8" />
+	<meta
+		content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
+		name="viewport"
+	/>
+</svelte:head>
+<slot />
+
+<style lang="scss" global>
+	@import '../styles/root/global.scss';
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import path from "path";
+
+export default defineConfig({
+	plugins: [sveltekit()]
+});
