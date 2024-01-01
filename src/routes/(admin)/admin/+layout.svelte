89d828c007547f3f49dac5d5fbc2ae58c56ef2d0
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-01 20:10:05 +0530

    feat: admin dashboard init

---
+++ src/routes/(admin)/admin/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Sidebar from '$components/Sidebar.svelte';
+	import type { PageData } from './$types';
+	import { ADMIN_ROUTES } from '$lib/routes';
+	export let data: PageData;
+</script>
+
+<main class="AdminLayout">
+	<Sidebar
+		user={{
+			email: data.email,
+			userId: data.userId
+		}}
+		routes={ADMIN_ROUTES}
+	/>
+
+	<div class="AdminLayout__content">
+		<slot />
+	</div>
+</main>
+
+<style lang="scss">
+	.AdminLayout {
+		@include box();
+		display: grid;
+		grid-template-columns: min-content 1fr;
+		grid-template-areas: 'sidebar content';
+	}
+</style>
