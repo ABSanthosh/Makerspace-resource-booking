901f7a53f4081603a7761c1da14c5fd7058eac31
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-07 06:19:22 +0530

    feat: client side auth subscription

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
-	import { derived, writable } from 'svelte/store';
+	import { writable } from 'svelte/store';
 
 	export let data;
 
@@ -10,23 +10,15 @@
 	let supabaseStore = writable<typeof supabase>();
 	$: supabaseStore.set(supabase);
 
-	// this is necessary to ensure that subscriptions to old supabase clients are cleaned up properly
-	// when a new client is retrieved from the loader.
-	let supabaseAuthStateChangeSubscriptionStore = derived(supabaseStore, ($supabaseStore, set) => {
+	onMount(() => {
 		const {
 			data: { subscription }
-		} = $supabaseStore.auth.onAuthStateChange((_, _session) => {
+		} = supabase.auth.onAuthStateChange((_, _session) => {
 			if (_session?.expires_at !== session?.expires_at) {
 				invalidate('supabase:auth');
 			}
 		});
-		set(subscription);
-		return subscription.unsubscribe;
-	});
-
-	onMount(() => {
-		/* eslint-disable-next-line @typescript-eslint/no-empty-function */
-		return supabaseAuthStateChangeSubscriptionStore.subscribe(() => {});
+		return () => subscription.unsubscribe();
 	});
 </script>
 
