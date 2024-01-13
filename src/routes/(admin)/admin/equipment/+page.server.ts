3ff12405ff0220940b247c7d425d0c93dfc52bae
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-13 23:47:49 +0530

    chore: added add and edit actions

--- src/routes/(admin)/admin/equipment/+page.server.ts
+++ src/routes/(admin)/admin/equipment/+page.server.ts
@@ -1,20 +1,50 @@
 import { superValidate } from 'sveltekit-superforms/server';
 import type { PageServerLoad } from './$types';
 import { EZodSchema } from '$lib/schemas';
-import { getAllEquipment, getECategories } from '$db/Equipment.db';
+import { addEquipment, editEquipment, getAllEquipment, getECategories } from '$db/Equipment.db';
+import { fail, type Actions } from '@sveltejs/kit';
 
 // @ts-ignore
 export const load: PageServerLoad = async ({ locals }) => {
-  const newEquipmentForm = await superValidate(EZodSchema);
-  const editEquipmentForm = await superValidate(EZodSchema);
-  const allEquipment = await getAllEquipment();
-  const eCategories = await getECategories();
-  if (Object.keys(locals).length === 0) return { newEquipmentForm };
+	const newEquipmentForm = await superValidate(EZodSchema);
+	const editEquipmentForm = await superValidate(EZodSchema);
+	const allEquipment = await getAllEquipment();
+	const eCategories = await getECategories();
+	if (Object.keys(locals).length === 0) return { newEquipmentForm };
 
-  return {
-    newEquipmentForm,
-    editEquipmentForm,
-    allEquipment,
-    eCategories
-  };
+	return {
+		newEquipmentForm,
+		editEquipmentForm,
+		allEquipment,
+		eCategories
+	};
+};
+
+export const actions: Actions = {
+	add: async ({ request }) => {
+		const newEquipmentForm = await superValidate(request, EZodSchema);
+		if (!newEquipmentForm.valid) {
+			return fail(400, { newEquipmentForm });
+		}
+		return {
+			form: {
+				...newEquipmentForm,
+				response: await addEquipment(newEquipmentForm.data),
+				allEquipment: await getAllEquipment()
+			}
+		};
+	},
+	edit: async ({ request }) => {
+		const editEquipmentForm = await superValidate(request, EZodSchema);
+		if (!editEquipmentForm.valid) {
+			return fail(400, { editEquipmentForm });
+		}
+		return {
+			form: {
+				...editEquipmentForm,
+				response: await editEquipment(editEquipmentForm.data),
+				allEquipment: await getAllEquipment()
+			}
+		};
+	}
 };
