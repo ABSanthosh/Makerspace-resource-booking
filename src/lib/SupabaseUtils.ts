7b1ffc569340f33b8857272f511cd300e5892382
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-23 20:41:27 +0530

    chore: generalized user profile check util

--- src/lib/SupabaseUtils.ts
+++ src/lib/SupabaseUtils.ts
@@ -4,7 +4,7 @@ import { SupabaseEnum } from './Enums';
 
 export async function isAdmin(supabase: SupabaseClient<any>, userId: string) {
 	return (
-		(await supabase.from('profile').select('*').eq('id', userId).single()).data?.role === 'admin'
+		(await supabase.from('profile').select('*').eq('id', userId).single()).data
 	);
 }
 
