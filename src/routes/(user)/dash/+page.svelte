31b8da51c21b6742da50307b7d19403adcf8ff36
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-23 20:40:10 +0530

    chore: dashboard layout

--- src/routes/(user)/dash/+page.svelte
+++ src/routes/(user)/dash/+page.svelte
@@ -1,3 +1,26 @@
-<h1>This is a protected page.</h1>
+<script lang="ts">
+	import { SessionStore } from '$store/SupaStore';
 
-<a href="../">Go back</a>
+	$: user = $SessionStore!.user;
+</script>
+
+<main class="Dashboard">
+	<h1>
+		Hello, {user.user_metadata.name}!
+	</h1>
+</main>
+
+<style lang="scss">
+	.Dashboard {
+		gap: 64px;
+		@include box();
+		margin: 64px auto;
+		max-width: $maxDashWidth;
+		@include make-flex($just: flex-start, $align: flex-start);
+
+		& > h1 {
+			font-size: 30px;
+			font-weight: 500;
+		}
+	}
+</style>
