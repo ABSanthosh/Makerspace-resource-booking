74e5dec7ab252894b76841520b5417486ae6a7ed
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-28 01:16:29 +0530

    chore: fixed parent data load type

--- src/routes/(admin)/admin/equipment/+page.server.ts
+++ src/routes/(admin)/admin/equipment/+page.server.ts
@@ -6,16 +6,16 @@ import { fail, type Actions } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
 import { addEquipment, editEquipment, getAllEquipment, getECategories } from '$db/Equipment.db';
 
-// @ts-ignore
 export const load: PageServerLoad = async ({ locals }) => {
 	const newEquipmentForm = await superValidate(EZodSchema);
-	if (Object.keys(locals).length === 0) return { newEquipmentForm };
+	if (Object.keys(locals).length === 0) return { newEquipmentForm, ...locals };
 
 	return {
 		newEquipmentForm,
 		editEquipmentForm: await superValidate(EZodSchema),
 		allEquipment: await getAllEquipment(),
-		eCategories: await getECategories()
+		eCategories: await getECategories(),
+		...locals
 	};
 };
 
