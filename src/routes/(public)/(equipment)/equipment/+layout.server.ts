cdf7ff039ad3ac95ef9bba78d2ca2c421e4ba685
ABSanthosh <a.b.santhosh02@gmail.com>
2024-01-28 01:16:36 +0530

    chore: fixed parent data load type

--- src/routes/(public)/(equipment)/equipment/+layout.server.ts
+++ src/routes/(public)/(equipment)/equipment/+layout.server.ts
@@ -1,10 +1,10 @@
 import { getAllEquipmentPreview, getECategories } from '$db/Equipment.db.js';
 import type { LayoutServerLoad } from './$types';
 
-// @ts-ignore
-export const load: LayoutServerLoad = async ({}) => {
+export const load: LayoutServerLoad = async ({ locals }) => {
 	return {
 		categories: await getECategories(),
-		allEquipment: await getAllEquipmentPreview()
+		allEquipment: await getAllEquipmentPreview(),
+		...locals
 	};
 };
