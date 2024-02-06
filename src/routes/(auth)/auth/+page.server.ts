db96f6b39104db9ffac2d02705c76e990c2cf4d9
ABSanthosh <a.b.santhosh02@gmail.com>
2024-02-07 01:43:37 +0530

    feat: new logout route

---
+++ src/routes/(auth)/auth/+page.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+  logout: async ({locals: {supabase}}) => {
+    console.log("logging out");
+    await supabase.auth.signOut();
+    throw redirect(303, "/");
+  }
+}
