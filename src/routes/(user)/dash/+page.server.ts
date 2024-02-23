d1160db4fcc3647ecf0687c960fca1b31a25009c
ABSanthosh <a.b.santhosh02@gmail.com>
2024-02-24 00:24:33 +0530

    chore: prefilled forms

--- src/routes/(user)/dash/+page.server.ts
+++ src/routes/(user)/dash/+page.server.ts
@@ -1,30 +1,26 @@
 import type { Actions, PageServerLoad } from './$types';
 import { getUserProfile, updateUserProfile } from '$db/User.db';
-import { UserProfileZodSchema } from '$lib/schemas';
+import { UserProfileZodSchema, type UserProfileSchema } from '$lib/schemas';
 import { superValidate } from 'sveltekit-superforms/server';
 import { zod } from 'sveltekit-superforms/adapters';
+import type { ProfileType } from '@prisma/client';
 
 // @ts-ignore
 export const load: PageServerLoad = async ({ locals }) => {
-	const userProfileForm = await superValidate(zod(UserProfileZodSchema));
-	// console.log(userProfileForm);
+	let userProfileForm = await superValidate(zod(UserProfileZodSchema));
 	if (Object.keys(locals).length === 0) return { userProfileForm };
-
-	// TODO: change to use v2 of superForms
 	const dbUserProfile = await getUserProfile(locals.session?.user.id!);
-	userProfileForm.data = {
-		...userProfileForm.data,
-		name: dbUserProfile?.name ?? '',
-		email: dbUserProfile?.email ?? '',
-		mobile: dbUserProfile?.mobile ?? '',
-		branch: dbUserProfile?.branch ?? '',
-		department: dbUserProfile?.department ?? '',
-		year: dbUserProfile?.year ?? 0,
-		isNew: dbUserProfile?.isNew!,
-		role: dbUserProfile?.role!,
-		userId: dbUserProfile?.userId ?? '',
-		clubs: (dbUserProfile?.clubs as string[]) ?? []
-	};
+	userProfileForm = await superValidate(
+		{
+			name: dbUserProfile?.name,
+			email: dbUserProfile?.email,
+			mobile: dbUserProfile?.mobile || '',
+			isNew: dbUserProfile?.isNew,
+			type: dbUserProfile?.type as ProfileType,
+			typeData: dbUserProfile?.typeData as UserProfileSchema['typeData']
+		},
+		zod(UserProfileZodSchema)
+	);
 
 	return {
 		userProfileForm,
@@ -40,20 +36,20 @@ export const actions: Actions = {
 			return { userProfileForm };
 		}
 
-		const newProfileData = await updateUserProfile({
-			...userProfileForm.data,
-			isNew: false,
-			id: session?.user.id!
-		});
+		// const newProfileData = await updateUserProfile({
+		// 	...userProfileForm.data,
+		// 	isNew: false,
+		// 	id: session?.user.id!
+		// });
 
 		// When the user updates their profile, we need to refresh the session
-		if (userProfileForm.data.isNew !== newProfileData.isNew) {
-			supabase.auth.refreshSession();
-			session = await getSession();
-		}
-		return {
-			userProfileForm,
-			profileData: newProfileData
-		};
+		// if (userProfileForm.data.isNew !== newProfileData.isNew) {
+		// 	supabase.auth.refreshSession();
+		// 	session = await getSession();
+		// }
+		// return {
+		// 	userProfileForm,
+		// 	profileData: newProfileData
+		// };
 	}
 };
